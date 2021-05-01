a7d249178e1d1f7bd3fe0e29982ea45ee455d67e
shanemcf <shanemcfadden95@yahoo.com>
2021-05-01 08:32:52 -0500

    created request for excersize list and messing with api data

--- assets/script.js
+++ assets/script.js
@@ -26,17 +26,31 @@ const postWorkout = async () => {
         username: "test",
         password: "workout",
         workout: "workout input"
+        
     }
     
     const result = await
     fetch('api/workout', {method: 'POST', body: JSON.stringify(payload)});
     const data = result.json();
-    console.log(payload);
+    
     return data
     
 }
+console.log(postWorkout);
+
 document.querySelector('#submit').addEventListener('click', () => {postWorkout();
+
     // document.querySelector('#submit').addEventListener('click', postWorkout)
+
+    // possible change for form submission
+    // function logSubmit(event) {
+    //     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    //     event.preventDefault();
+    //   }
+      
+    //   const form = document.getElementById('form');
+    //   const log = document.getElementById('log');
+    //   form.addEventListener('submit', logSubmit);
 })
 
 //  <script>
@@ -59,3 +73,29 @@ fetch(" https://wger.de/api/v2/schedule", obj)
 
     /* </script> */
     
+
+    //practice for drop-down list of excersizes
+
+    // async function getEx() {
+    //     let url = 'https://wger.de/api/v2/exercise/?format=json'
+    //     const array = [];
+  
+    //     while (url) {
+    //       const res = await fetch(url)
+    //       data = await res.json()
+    //       for (const item of data.results) {
+    //           console.log(item.name)
+    //           array.push(item.name);
+    //       }
+    //       url = data.next
+    //   }
+    //   return array;
+    //   }
+    //   $(function() {
+    //     let tags = [];
+    //     getEx().then(res => {
+    //       $( "#tags" ).autocomplete({
+    //       source: res
+    //     });
+    //     });
+    //   } );
