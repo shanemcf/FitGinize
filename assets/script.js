6f34fb50852c49a6d5aea65a9489b242e752032f
shanemcf <shanemcfadden95@yahoo.com>
2021-05-01 12:12:09 -0500

    added a drop down menu of all excercises

--- assets/script.js
+++ assets/script.js
@@ -20,82 +20,135 @@
 
 // }
 
-
 const postWorkout = async () => {
-    const payload = {
-        username: "test",
-        password: "workout",
-        workout: "workout input"
-        
-    }
-    
-    const result = await
-    fetch('api/workout', {method: 'POST', body: JSON.stringify(payload)});
-    const data = result.json();
-    
-    return data
-    
+  const data = {
+    username: "test",
+    password: "workout",
+    workout: "workout input",
+  };
+
+  const result = await fetch("api/newApiEndpoint", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const data1 = result.json();
+
+  return data1;
+};
+
+const storedWorkoutInfo = () => {
+
 }
-console.log(postWorkout);
 
-document.querySelector('#submit').addEventListener('click', () => {postWorkout();
+document.querySelector("#submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target)
+  storedWorkoutInfo();
+});
+  // document.querySelector('#submit').addEventListener('click', postWorkout)
 
-    // document.querySelector('#submit').addEventListener('click', postWorkout)
+  // possible change for form submission
+  // function logSubmit(event) {
+  //     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+  //     event.preventDefault();
+  //   }
 
-    // possible change for form submission
-    // function logSubmit(event) {
-    //     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-    //     event.preventDefault();
-    //   }
-      
-    //   const form = document.getElementById('form');
-    //   const log = document.getElementById('log');
-    //   form.addEventListener('submit', logSubmit);
-})
+  //   const form = document.getElementById('form');
+  //   const log = document.getElementById('log');
+  //   form.addEventListener('submit', logSubmit);
+
 
 //  <script>
-var obj = {
-    method: "GET",
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
-        Origin: "",
-    },
+// var obj = {
+//     method: "GET",
+//     headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//         Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
+//         Origin: "",
+//     },
+// };
+// fetch(" https://wger.de/api/v2/workoutlog", obj)
+//     .then((res) => res.json())
+//     .then((data) => console.log("data: ", data));
+
+let data = {
+  reps: [1],
+  weight: [20],
+  date: ["wed"],
+  exercise: ["curl"],
+  workout: [1],
 };
-fetch(" https://wger.de/api/v2/workoutlog", obj)
-    .then((res) => res.json())
-    .then((data) => console.log("Data: ", data));
 
-fetch(" https://wger.de/api/v2/schedule", obj)
-    .then((res) => res.json())
-    .then((data) => console.log("Data: ", data));
+let newObj = {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
+    Origin: "",
+  },
+  body: JSON.stringify(data),
+};
 
-    /* </script> */
-    
+//POst Route
+fetch(" https://wger.de/api/v2/workoutlog", newObj)
+  .then((res) => {
+    console.log(res);
+    res.json();
+  })
+  .then((data) => console.log("data: ", data));
 
-    //practice for drop-down list of excersizes
+//data needed back( in array?)
+// {
+//     "reps": [
+//         "This field is required."
+//     ],
+//     "weight": [
+//         "This field is required."
+//     ],
+//     "date": [
+//         "This field is required."
+//     ],
+//     "exercise": [
+//         "This field is required."
+//     ],
+//     "workout": [
+//         "This field is required."
+//     ]
+// }
 
-    // async function getEx() {
-    //     let url = 'https://wger.de/api/v2/exercise/?format=json'
-    //     const array = [];
-  
-    //     while (url) {
-    //       const res = await fetch(url)
-    //       data = await res.json()
-    //       for (const item of data.results) {
-    //           console.log(item.name)
-    //           array.push(item.name);
-    //       }
-    //       url = data.next
-    //   }
-    //   return array;
-    //   }
-    //   $(function() {
-    //     let tags = [];
-    //     getEx().then(res => {
-    //       $( "#tags" ).autocomplete({
-    //       source: res
-    //     });
-    //     });
-    //   } );
+/* </script> */
+
+//practice for drop-down list of excersizes
+
+async function getEx() {
+  let url = "https://wger.de/api/v2/exercise/?format=json";
+  const array = [];
+
+  while (url) {
+    const res = await fetch(url);
+    data = await res.json();
+    for (const item of data.results) {
+      //   console.log(item.name);
+      array.push(item.name);
+    }
+    url = data.next;
+  }
+  //   console.log(array);
+  return array;
+}
+
+getEx().then((data) => {
+  console.log(data);
+         //populate drop down with data
+  var select = document.getElementById("exerciseList");
+
+  for (let i = 0; i < data.length; i++) {
+    var opt = data[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }
+});
