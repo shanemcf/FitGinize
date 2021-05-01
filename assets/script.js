12b3bb0ab86e52f25be07b27432f274c3dc325b7
shanemcf <shanemcfadden95@yahoo.com>
2021-05-01 18:32:19 -0500

    changed card to api 2 for better placement and added forms to be able to keep track for workout log

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,4 @@
-// Wger APi
 
-// var wLog = document.getElementById("Develop-1-row");
-
-//name functions for api
-// function workoutLog() {
-
-//fetch workout
-// fetch (
-//     `https://wger.de/api/v2/workoutlog/`
-// )
-
-// .then(function(logResponse) {
-//     return logResponse.json();
-// })
-// console.log(logResponse);
-
-//.then
-// take in user information
-
-// }
 
 const postWorkout = async () => {
   const data = {
@@ -45,7 +25,7 @@ document.querySelector("#submit").addEventListener("click", (event) => {
     console.log(event.target)
   storedWorkoutInfo();
 });
-  // document.querySelector('#submit').addEventListener('click', postWorkout)
+  
 
   // possible change for form submission
   // function logSubmit(event) {
@@ -58,19 +38,7 @@ document.querySelector("#submit").addEventListener("click", (event) => {
   //   form.addEventListener('submit', logSubmit);
 
 
-//  <script>
-// var obj = {
-//     method: "GET",
-//     headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//         Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
-//         Origin: "",
-//     },
-// };
-// fetch(" https://wger.de/api/v2/workoutlog", obj)
-//     .then((res) => res.json())
-//     .then((data) => console.log("data: ", data));
+
 
 let data = {
   reps: [1],
@@ -130,12 +98,10 @@ async function getEx() {
     const res = await fetch(url);
     data = await res.json();
     for (const item of data.results) {
-      //   console.log(item.name);
       array.push(item.name);
     }
     url = data.next;
   }
-  //   console.log(array);
   return array;
 }
 
@@ -152,3 +118,19 @@ getEx().then((data) => {
     select.appendChild(el);
   }
 });
+
+
+     
+      //Get Method for workout Log not pulling the right data
+// var obj = {
+//     method: "GET",
+//     headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//         Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
+//         Origin: "",
+//     },
+// };
+// fetch(" https://wger.de/api/v2/workoutlog", obj)
+//     .then((res) => res.json())
+//     .then((data) => console.log("data: ", data));
