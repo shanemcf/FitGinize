11a53ca4bec22065840171d794be22c11ee4a5f4
shanemcf <shanemcfadden95@yahoo.com>
2021-05-01 19:42:40 -0500

    logging data values from form

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ const postWorkout = async () => {
     workout: "workout input",
   };
 
-  const result = await fetch("api/newApiEndpoint", {
+  const result = await fetch("api/workoutlog", {
     method: "POST",
     body: JSON.stringify(data),
   });
@@ -27,6 +27,45 @@ document.querySelector("#submit").addEventListener("click", (event) => {
 });
   
 
+
+async function getEx() {
+  let url = "https://wger.de/api/v2/exercise/?format=json";
+  const array = [];
+
+  while (url) {
+    const res = await fetch(url);
+    data = await res.json();
+    for (const item of data.results) {
+      array.push(item.name);
+    }
+    url = data.next;
+  }
+  return array;
+}
+
+getEx().then((data) => {
+         //populate drop down with data
+  var select = document.getElementById("exerciseList");
+
+  for (let i = 0; i < data.length; i++) {
+    var opt = data[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }
+});
+  //  getting the data after submission
+  document.querySelector("#submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    data = {}
+    data.reps = document.getElementById("reps").value;
+    data.weight = document.getElementById("weight").value;
+    data.sets = document.getElementById("sets").value;
+    
+    console.log(data.weight);
+  })
+
   // possible change for form submission
   // function logSubmit(event) {
   //     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
@@ -39,33 +78,6 @@ document.querySelector("#submit").addEventListener("click", (event) => {
 
 
 
-
-let data = {
-  reps: [1],
-  weight: [20],
-  date: ["wed"],
-  exercise: ["curl"],
-  workout: [1],
-};
-
-let newObj = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
-    Origin: "",
-  },
-  body: JSON.stringify(data),
-};
-
-//POst Route
-fetch(" https://wger.de/api/v2/workoutlog", newObj)
-  .then((res) => {
-    console.log(res);
-    res.json();
-  })
-  .then((data) => console.log("data: ", data));
 
 //data needed back( in array?)
 // {
@@ -88,36 +100,6 @@ fetch(" https://wger.de/api/v2/workoutlog", newObj)
 
 /* </script> */
 
-//practice for drop-down list of excersizes
-
-async function getEx() {
-  let url = "https://wger.de/api/v2/exercise/?format=json";
-  const array = [];
-
-  while (url) {
-    const res = await fetch(url);
-    data = await res.json();
-    for (const item of data.results) {
-      array.push(item.name);
-    }
-    url = data.next;
-  }
-  return array;
-}
-
-getEx().then((data) => {
-  console.log(data);
-         //populate drop down with data
-  var select = document.getElementById("exerciseList");
-
-  for (let i = 0; i < data.length; i++) {
-    var opt = data[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
-  }
-});
 
 
      
@@ -134,3 +116,32 @@ getEx().then((data) => {
 // fetch(" https://wger.de/api/v2/workoutlog", obj)
 //     .then((res) => res.json())
 //     .then((data) => console.log("data: ", data));
+
+
+    //hard-coded method to look at data for POST
+// let data = {
+//   reps: [1],
+//   weight: [20],
+//   date: ["wed"],
+//   exercise: ["curl"],
+//   workout: [1],
+// };
+  // POST method having issues at this endpoint
+// let newObj = {
+//   method: "POST",
+//   headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//     Authorization: "Token " + "22cf1decb1a1c1f8492379d41682e3345b0dec74",
+//     Origin: "",
+//   },
+//   body: JSON.stringify(data),
+// };
+
+//POST Route, also having issues possibly due to endpoint
+// fetch(" https://wger.de/api/v2/workoutlog", newObj)
+//   .then((res) => {
+//     console.log(res);
+//     res.json();
+//   })
+//   .then((data) => console.log("data: ", data));
